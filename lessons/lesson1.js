691866b70844fc1b7001bcdbc80d542bf6c9c4af
RossKoll <rosko48@gmail.com>
2025-01-06 22:55:19 +0200

    second commit , added : 1. Concatenation 2. Interpolation

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -1,5 +1,3 @@
-
-
 // 1. hello world
 console.log("hello world via JavaScript");
 
@@ -13,4 +11,14 @@ var emptyVar = null;
 var undefinedVar;
 console.log("empryVar: " + typeof emptyVar);
 console.log("undefinedVar: " + typeof undefinedVar);
+
+// 4. Concatenation and Interpolation
+var price = 50
+var itemName = "Cup"
+var message = "The price of your " + itemName + " is " + price + " dollars"; // concatenation
+console.log(message);
+
+var messageToPrint = `The price of your ${itemName} is ${price} dollars`; // interpolation
+console.log(messageToPrint);
+
 
