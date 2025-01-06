d5d503785bc4b627b2fd642167fdbac32a0a6c84
Админ <rosko48@gmail.com>
2025-01-06 21:47:57 +0200

    first commit

---
+++ lessons/lesson1.js
@@ -0,0 +1,16 @@
+
+
+// 1. hello world
+console.log("hello world via JavaScript");
+
+// 2. variables and data types
+var firstName = "John";
+let lastName = "Doe";
+console.log("hello my dear frind " + firstName + " " + lastName);
+
+// 3.data types
+var emptyVar = null;
+var undefinedVar;
+console.log("empryVar: " + typeof emptyVar);
+console.log("undefinedVar: " + typeof undefinedVar);
+
